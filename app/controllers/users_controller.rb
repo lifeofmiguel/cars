c261470832e60c57090523fbf9ebbe5d32643c9a
Miguel Angel Morales <mmorales21500@live.com>
2021-03-24 13:05:39 -0400

    added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < Sinatra::Base
 
     get '/signup' do
+        erb :"users/new"
     end
 
     post '/signup'do
+        user = User.new(params["user"])
+        if user.save
+            session["user_id"] = user.id
+            redirect "/"
+        else
+            redirect "signup"
+        end
     end
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
 require_relative './config/environment'
 
 use Rack::MethodOverride
+
+use UsersController
+use SessionsController
 run ApplicationController
